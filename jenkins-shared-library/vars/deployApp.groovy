b56336b64ee462ee32a0c4e8d0632375a80b8fd7
Thanikai <128052851+ThanikaiCloudevops@users.noreply.github.com>
2026-06-24 16:22:09 +0530

    Add files via upload

---
+++ jenkins-shared-library/vars/deployApp.groovy
@@ -0,0 +1,15 @@
+def call(){
+    sh '''
+    
+        sudo systemctl daemon-reload
+
+        sudo systemctl restart sample-application
+
+        sleep 20
+
+        sudo systemctl status sample-application  --no-pager
+
+        docker ps
+    
+    '''
+}
